8407f9a25fc7b49469c6bbdd43b0878ad7439770
Vaibhavi-chinchure <85163791+Vaibhavi-chinchure@users.noreply.github.com>
2025-10-24 22:36:25 +0530

    Update db.js

--- src/config/db.js
+++ src/config/db.js
@@ -4,26 +4,27 @@ import fs from "fs";
 
 dotenv.config();
 
-const fs= require('fs');
 const pool = mysql.createPool({
   host: 'gateway01.ap-northeast-1.prod.aws.tidbcloud.com',
   user: '3io6ZMytZpS2mdq.root',
   password: 'HDSNA0OluDsCXNWV',
   database: 'healthcop',
-  ssl: {
-    rejectUnauthorized: false,
-    ca: fs.readFileSync(process.env.CA),
-    servername: undefined
-  }
+  ssl: process.env.CA
+    ? {
+        rejectUnauthorized: false,
+        ca: fs.readFileSync(process.env.CA),
+        servername: undefined,
+      }
+    : { rejectUnauthorized: false },
 });
 
 pool.getConnection()
   .then(connection => {
-    console.log("Database connected successfully");
+    console.log("✅ Database connected successfully");
     connection.release();
   })
   .catch(err => {
-    console.error("Database connection failed:", err.message);
+    console.error("❌ Database connection failed:", err.message);
   });
 
 export default pool;
