7212d1d16c400443479b92d97d0f50218122d9c2
Vaibhavi-chinchure <85163791+Vaibhavi-chinchure@users.noreply.github.com>
2025-10-24 14:20:15 +0530

    Update db.js

--- src/config/db.js
+++ src/config/db.js
@@ -1,13 +1,15 @@
 const mysql = require("mysql2/promise");
 require("dotenv").config();
 
+const fs= require('fs');
 const pool = mysql.createPool({
-  host: 'gateway01.ap-northeast-1.prod.aws.tidbcloud.com',
-  user: '3io6ZMytZpS2mdq.root',
-  password: 'HDSNA0OluDsCXNWV',
+  host: '192.168.1.6',
+  user: 'vaibhavi',
+  password: '@Vaibhavi143',
   database: 'healthcop',
-   ssl: {
-    rejectUnauthorized: true
+ ssl: {
+    rejectUnauthorized: false,
+    ca: fs.readFileSync(process.env.CA)
   }
 });
 
