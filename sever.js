7dda0c4e3dcd51ac56b1c330e9eb7388ac5374fe
Vaibhavi-chinchure <85163791+Vaibhavi-chinchure@users.noreply.github.com>
2025-10-27 16:11:12 +0530

    Update sever.js

--- sever.js
+++ sever.js
@@ -1,5 +1,4 @@
-
-import serverless from "serverless-http";  // or require('serverless-http') if using CommonJS
+  // or require('serverless-http') if using CommonJS
 import app from "./app.js";                // adjust path if needed
 
 // Wrap Express app as a Vercel Serverless Function
@@ -7,3 +6,4 @@ export const handler = serverless(app);
 
 // Optional default export (for local dev or testing)
 export default app;
+
